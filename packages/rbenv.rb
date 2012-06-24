2f9bfe5847c75a02d62100e7fede4f422be658fc
Francis Siefken <fsiefken@gmail.com>
2012-06-24 17:51:20 +0300

    updated to latest ruby version 1.9.3-p194 (2012-05-20)

--- packages/rbenv.rb
+++ packages/rbenv.rb
@@ -21,13 +21,13 @@ package :symlink_rubies do
   requires :default_rubies
   
   runner "mkdir -p /home/#{DEPLOY_USER}/.rbenv/versions"
-  runner "ln -s /opt/ruby-1.9.2-p290 /home/#{DEPLOY_USER}/.rbenv/versions/1.9.2-p290"
+  runner "ln -s /opt/ruby-1.9.2-p290 /home/#{DEPLOY_USER}/.rbenv/versions/1.9.3-p194"
   runner "ln -s /opt/ruby-1.8.7-p352 /home/#{DEPLOY_USER}/.rbenv/versions/1.8.7-p352"
 
   verify do
     @commands << "sudo -u #{DEPLOY_USER} -i /home/#{DEPLOY_USER}/.rbenv/bin/rbenv versions | grep 1.8.7-p352"
-    @commands << "sudo -u #{DEPLOY_USER} -i /home/#{DEPLOY_USER}/.rbenv/bin/rbenv versions | grep 1.9.2-p290"
-    has_symlink "/home/#{DEPLOY_USER}/.rbenv/versions/1.9.2-p290", '/opt/ruby-1.9.2-p290' 
+    @commands << "sudo -u #{DEPLOY_USER} -i /home/#{DEPLOY_USER}/.rbenv/bin/rbenv versions | grep 1.9.3-p194"
+    has_symlink "/home/#{DEPLOY_USER}/.rbenv/versions/1.9.2-p290", '/opt/ruby-1.9.3-p194' 
     has_symlink "/home/#{DEPLOY_USER}/.rbenv/versions/1.8.7-p352", '/opt/ruby-1.8.7-p352' 
   end
 end
@@ -38,18 +38,18 @@ package :set_default_ruby do
   push_text '1.9.2-p290', "/home/#{DEPLOY_USER}/.rbenv/global"
   
   verify do
-    file_contains "/home/#{DEPLOY_USER}/.rbenv/global", '1.9.2-p290'
+    file_contains "/home/#{DEPLOY_USER}/.rbenv/global", '1.9.3-p194'
   end
 end
 
 package :add_rbenv_bundler do
   requires :config_gemrc
   
-  runner "/home/#{DEPLOY_USER}/.rbenv/versions/1.9.2-p290/bin/gem install bundler"
+  runner "/home/#{DEPLOY_USER}/.rbenv/versions/1.9.3-p194/bin/gem install bundler"
   runner "/home/#{DEPLOY_USER}/.rbenv/bin/rbenv rehash"
   
   verify do 
-    @commands << "/home/#{DEPLOY_USER}/.rbenv/versions/1.9.2-p290/bin/gem list | grep bundler"
+    @commands << "/home/#{DEPLOY_USER}/.rbenv/versions/1.9.3-p194/bin/gem list | grep bundler"
   end
 end
 
